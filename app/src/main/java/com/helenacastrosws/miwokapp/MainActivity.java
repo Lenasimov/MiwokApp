776dac04ce9534fa6c9b96cac2135f4e341501d8
lenac <lenaccastro92@gmail.com>
2018-10-12 23:33:27 -0300

    Using ListView + ArrayAdapter to show Numbers lists;

--- app/src/main/java/com/helenacastrosws/miwokapp/MainActivity.java
+++ app/src/main/java/com/helenacastrosws/miwokapp/MainActivity.java
@@ -1,7 +1,8 @@
 package com.helenacastrosws.miwokapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
@@ -31,28 +32,31 @@ public class MainActivity extends AppCompatActivity {
         numbers.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent numbersIntent = new Intent(MainActivity.this, NumbersActivity.class);
+                startActivity(numbersIntent);
             }
         });
-
         family.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent familyIntent = new Intent(MainActivity.this, FamilyActivity.class);
+                startActivity(familyIntent);
             }
         });
 
         colors.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent colorsIntent = new Intent(MainActivity.this, ColorsActivity.class);
+                startActivity(colorsIntent);
             }
         });
 
         phrases.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent phrasesIntent = new Intent(MainActivity.this, PhrasesActivity.class);
+                startActivity(phrasesIntent);
             }
         });
     }
--- app/src/main/java/com/helenacastrosws/miwokapp/NumbersActivity.java
+++ app/src/main/java/com/helenacastrosws/miwokapp/NumbersActivity.java
@@ -1,13 +1,44 @@
 package com.helenacastrosws.miwokapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class NumbersActivity extends AppCompatActivity {
+
+    public static final String TAG = "NumbersActivity";
+
+    @BindView(R.id.root_list_view)
+    ListView numbersListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+        ButterKnife.bind(this);
+
+        ArrayList<String> words = new ArrayList<>();
+        words.add("One");
+        words.add("Two");
+        words.add("Three");
+        words.add("Four");
+        words.add("Five");
+        words.add("Six");
+        words.add("Seven");
+        words.add("Eight");
+        words.add("Nine");
+        words.add("Ten");
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, words);
+        numbersListView.setAdapter(adapter);
     }
+
 }
